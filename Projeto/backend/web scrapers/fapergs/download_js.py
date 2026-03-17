f5e59f71ff209ff430d1d6560c52646855050ea6
lorenzo <lorenzozajaczkowskimartins@gmail.com>
2026-03-17 19:55:56 -0300

    apenas removi um arquivo com coisas duplicadas

--- Projeto/backend/web scrapers/fapergs/download_js.py
+++
@@ -1,5 +0,0 @@
-import requests
-url="https://fapergs.rs.gov.br/matriz_common/versions/2.1.5/js/procergs/jquery.matrizPagedList.js?2.1.5.156.5+7"
-r=requests.get(url)
-print('status', r.status_code)
-print(r.text[:3000])
